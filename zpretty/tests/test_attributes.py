a866b3b16204929ee269ca27b36d63edb1ce79da
ale-rt <alessandro.pisa@gmail.com>
2021-01-02 15:14:05 +0100

    Rendering refactoring
    The attributes renderer knows about the element indentation
    and for indents the attributes consequently.
    The ZCML element has now its custom tag templates, this simplifies the code
    Attributes content spanning multiple lines is not indented anymore (Closes #17)

--- zpretty/tests/test_attributes.py
+++ zpretty/tests/test_attributes.py
@@ -1,21 +1,46 @@
+from bs4 import BeautifulSoup
 from unittest import TestCase
 from zpretty.attributes import PrettyAttributes
+from zpretty.elements import PrettyElement
 
 
 class TestZPrettyAttributess(TestCase):
     """Test zpretty"""
 
+    def get_element(self, text, level=0):
+        """Given a text return a PrettyElement"""
+        soup = BeautifulSoup(
+            "<soup><fake_root>%s</fake_root></soup>" % text, "html.parser"
+        )
+        return PrettyElement(soup.fake_root.next_element, level)
+
     def assertPrettifiedAttributes(self, attributes, expected, level=0):
         """Check if the attributes are properly sorted and formatted"""
-        pretty_attribute = PrettyAttributes(attributes)
+        if level == 0:
+            el = None
+        else:
+            el = self.get_element("a", level)
+        pretty_attribute = PrettyAttributes(attributes, el)
         observed = pretty_attribute()
         self.assertEqual(observed, expected)
 
     def test_no_attributes(self):
         self.assertPrettifiedAttributes({}, "")
+        self.assertPrettifiedAttributes({}, "", level=2)
 
     def test_one_attribute(self):
         self.assertPrettifiedAttributes({u"a": "1"}, 'a="1"')
+        self.assertPrettifiedAttributes({u"a": "1"}, 'a="1"', level=1)
+        self.assertPrettifiedAttributes({u"a": "1"}, 'a="1"', level=2)
+
+    def test_many_attributes_attribute(self):
+        self.assertPrettifiedAttributes({u"a": "1", u"b": "2"}, 'a="1"\nb="2"')
+        self.assertPrettifiedAttributes(
+            {u"a": "1", u"b": "2"}, '    a="1"\n    b="2"', level=1
+        )
+        self.assertPrettifiedAttributes(
+            {u"a": "1", u"b": "2"}, '      a="1"\n      b="2"', level=2
+        )
 
     def test_tal_define(self):
         self.assertPrettifiedAttributes(
@@ -25,12 +50,20 @@ class TestZPrettyAttributess(TestCase):
 
     def test_format_attributes_many_attribute(self):
         self.assertPrettifiedAttributes(
-            {u"a": "1", "b": "2", "class": "hidden", "tal:define": "a 1; b 2"},
+            {
+                u"a": "1",
+                "b": "2",
+                "class": "hidden",
+                "tal:define": "a 1; b 2",
+                "data-multiline": "foo\n   bar",
+            },
             "\n".join(
                 (
                     'class="hidden"',
                     'a="1"',
                     'b="2"',
+                    'data-multiline="foo',
+                    '   bar"',
                     'tal:define="',
                     "  a 1;",
                     "  b 2;",
