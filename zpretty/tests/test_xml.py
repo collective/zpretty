28ce9a75714af0d5f99caf9050a4eb6d5fcac546
Peter Mosmans <support@go-forward.net>
2022-09-28 13:00:47 +0200

    feat: add test case for XML CDATA support

--- zpretty/tests/test_xml.py
+++ zpretty/tests/test_xml.py
@@ -8,18 +8,21 @@ class TestZpretty(TestCase):
 
     maxDiff = None
 
-    def prettify(self, filename):
+    def prettify(self, filename, cdata):
         """Run prettify on filename and check that the output is equal to
         the file content itself
         """
         resolved_filename = resource_filename("zpretty.tests", "original/%s" % filename)
-        prettifier = XMLPrettifier(resolved_filename)
+        prettifier = XMLPrettifier(resolved_filename, cdata=cdata)
         observed = prettifier()
         expected = open(resolved_filename).read()
         self.assertListEqual(observed.splitlines(), expected.splitlines())
 
     def test_zcml(self):
-        self.prettify("sample_xml.xml")
+        self.prettify("sample_xml.xml", False)
 
     def test_sample_dtml(self):
-        self.prettify("sample_dtml.dtml")
+        self.prettify("sample_dtml.dtml", False)
+
+    def test_cdata(self):
+        self.prettify("sample_cdata.xml", True)
